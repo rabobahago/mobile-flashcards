6817e9447bbb6665d30b866ed011429b500775f7
rabo <rabobahago@gmail.com>
2021-12-16 14:50:06 +0100

    feat:navigation add to the project

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,27 @@
+import { createAppContainer, createStackNavigator } from "react-navigation";
+
+import AllDecksScreen from "../screens/AllDecks";
+import NewDeckScreen from "../screens/NewDeck";
+import NewCardScreen from "../screens/NewCard";
+import SingleDeckScreen from "../screens/SingleDeck";
+import QuizScreen from "../screens/Quiz";
+
+export default createAppContainer(
+  createStackNavigator({
+    AllDecks: {
+      screen: AllDecksScreen
+    },
+    NewDeck: {
+      screen: NewDeckScreen
+    },
+    SingleDeck: {
+      screen: SingleDeckScreen
+    },
+    NewCard: {
+      screen: NewCardScreen
+    },
+    Quiz: {
+      screen: QuizScreen
+    }
+  })
+);
