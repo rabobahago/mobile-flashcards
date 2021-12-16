9ecfa5c2ed5a48af97eedd37a3895e5a09901dc1
rabo <rabobahago@gmail.com>
2021-12-16 14:33:51 +0100

    feat:components folder create and styletext, tarbaricon components

---
+++ components/TarBarIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Icon } from "expo";
+
+import Colors from "../constants/Colors";
+
+const TabBarIcon = (props) => {
+  return (
+    <Icon.Ionicons
+      name={props.name}
+      size={26}
+      style={{ marginBottom: -3 }}
+      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+    />
+  );
+};
+
+export default TabBarIcon;
